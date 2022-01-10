c9842f28000d318bef765372169cb3f81da1634f
FofN <fofn@users.github.com>
2022-01-10 02:08:13 -0500

    jquery automation for project pages
    + project list and pages are now automatically filled using an xml doc and jquery, easier to add new projects
    
    todo
    - menu indicator
    - add more projects
    - styling improvements

---
+++ js/projectList.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    if (projectName == "index") {
+        $.ajax({
+            type: "GET" ,
+            url: "/xml/projects.xml" ,
+            dataType: "xml" ,
+            success: function(xml) {
+                $(xml).find('project').each(function(){
+    
+                    var uname = $(this).find('universalname').text(),
+                        name = $(this).find('name').text(),
+                        cat = $(this).find('category').text();
+    
+                    $("#project-list").append(
+    
+                        '<div data-aos="fade-in" class="col-xl-4 col-lg-6 col-md-6 col-sm-12 pl-1 pr-1 pb-2">' +
+                        '<a href="./projects/proj_' + uname + '/proj_' + uname + '.html">' +
+                        '<div id="proj-SkateSidescroller" class="proj">' +
+                        '<img class="proj-img" src="./images/thumbnail_' + uname + '.png" alt="Card image">' +
+                        '<div class="center-img-text">' +
+                        '<h1>' + name + '</h1>' +
+                        '<h4>(' + cat + ')</h4>' +
+                        '</div>' +
+                        '</div>' +
+                        '</a>' +
+                        '</div>'
+    
+                    );
+                }); 
+            }
+        });
+    }
+});
---
+++ js/projectPage.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    if (projectName != "index") {
+        $.ajax({
+            type: "GET" ,
+            url: "/xml/projects.xml" ,
+            dataType: "xml" ,
+            success: function(xml) {
+    
+                $(xml).find('project').each(function(){
+    
+                    if ($(this).find('universalname').text() == projectName) {
+                        var uname = $(this).find('universalname').text(),
+                            name = $(this).find('name').text(),
+                            desc = $(this).find('description').text();
+    
+                        document.title = 'JMM - ' + name;
+
+                        $("#navleft").append(
+    
+                            '<h1 class="name-proj">' + name.toLowerCase() + '</h1>' +
+                            '<a class="navlink navlink-left" href="/index.html">back to home</a>'
+        
+                        );
+    
+                        $("#proj-main-vid").append(
+    
+                            '<source src="/videos/vid_' + uname + '.mp4" type="video/mp4">' +
+                            'Your browser does not support the video tag.'
+        
+                        );
+    
+                        $("#project-details").append(
+    
+                            '<p data-aos="fade-up" class="heading pt-3">• ' + name.toLowerCase() + ' •</p>' +
+                            '<div data-aos="fade-up" class="row div-project-details">' +
+                            '<div class="col-lg col-md col-sm">' +
+                            '<div>' +
+                            '<h3 class="project-details-heading">Languages Used</h3>' +
+                            '<p>' + desc + '</p>' +
+                            '<h3 class="project-details-heading">Description</h3>' +
+                            '<p>' + desc + '</p>' +
+                            '</div>' +
+                            '</div>' +
+                            '</div>'
+        
+                        );
+                    }
+                }); 
+            }
+        });
+    }
+});
